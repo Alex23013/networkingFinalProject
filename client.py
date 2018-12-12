706f38710e0edc7934e2ccb331e7af012eea869a
Maria Cornejo <alecornejolupa2013@ucsp.edu.pe>
2018-12-12 02:04:06 -0500

    receiveMsg and sendMsg

--- client.py
+++ client.py
@@ -1,13 +1,20 @@
 import connection as con
- 
-s = con.connection("client")
+from protocol import (AVAILABLE_CODE, SEND_URLS_CODE, SEND_CRAWLED_DATA_CODE,
+                      ACKNOWLEDGE_CODE, ERROR_CODE) 
+
+s = con.connectionClient()
 initialMessage = input("Send: ")
 con.sendMsg (s,initialMessage)
  
 while True:
-  data = con.receiveMsg(s, 1024)
-  if data == None: break
-  print("Recieved: "+data)
+  msgType, size, data = con.receiveMsg(s)
+  if data == '': break
+  if msgType == AVAILABLE_CODE:
+    print("Recieved: "+msgType)
+  if msgType == ACKNOWLEDGE_CODE or msgType == ERROR_CODE:
+    print("Recieved: "+msgType+ "code: "+data)
+  if msgType == SEND_URLS_CODE or msgType == SEND_CRAWLED_DATA_CODE:
+    print("Recieved: "+msgType+ "size: "+ size + "content"+data)
 
   response = input("Reply: ")
   con.sendMsg (s,response)  
